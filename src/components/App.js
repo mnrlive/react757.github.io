03168c1c1b5b14592e7538cfa8a2662eb7ff2b1b
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-14 15:35:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -52,8 +52,8 @@ class App extends Component {
   render() {
     const routeItems = this.state.itemsAll.map((item, index) => (
       <Route exact path={`/${item.answer}`} key={index}
-        render={() => <main className="main main--country"><Country itemsAll={this.state.itemsAll} flag={item.image} name={item.question} nativeName={item.question}
-          region={item.question} subRegion={item.question} capital={item.question} topLevelDomain={item.question} currencies={item.question} languages={item.languages} borders={item.answer} alpha3Code={item.answer}
+        render={() => <main className="main main--country"><Country itemsAll={this.state.itemsAll} flag={item.image} name={item.question} 
+         borders={item.answer} alpha3Code={item.answer}
         /></main>} //zeby country wyswietlalo odpowiednie panstwo zwiazane z linkiem
       />
     ))
