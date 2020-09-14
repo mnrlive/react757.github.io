892c2203c0d56a89540f7ea02e351ac37901c91f
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-14 16:40:09 +0530

    Update Country.js

--- src/components/Country.js
+++ src/components/Country.js
@@ -32,7 +32,7 @@ const Country = (props) => {
                     <ul className="info-list info-list--partThree">
                         <ul className="info-list__borders">
                             <h4>Border Countries:</h4>
-                               <li className="borders-item" key={index}>
+                               <li className="borders-item" >
             <NavLink to= {props.borders} className="navLink">
                 <button className="btn btn--borders">{props.borders}</button>
             </NavLink>
