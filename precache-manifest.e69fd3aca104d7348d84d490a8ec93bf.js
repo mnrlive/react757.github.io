62efcd667f648d6bcd173521dbfb7d408bdc8083
Pandimadhubabu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-14 14:05:18 +0000

    Deploying to master from  @ 6d05b32bc161f2dc346fd81f0f16924af1fdbe9c 🚀

---
+++ precache-manifest.e69fd3aca104d7348d84d490a8ec93bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80907ae4d60792abb370",
+    "url": "/static/css/main.2ded1270.chunk.css"
+  },
+  {
+    "revision": "80907ae4d60792abb370",
+    "url": "/static/js/main.595cf09c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a26c87962c93f5cc058",
+    "url": "/static/js/2.83d4ad58.chunk.js"
+  },
+  {
+    "revision": "e4f85b1c64efaee95a9ee529a26b37e8",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1fb25c85e42c0982517284e7b4e9dd2.js"
+  "/precache-manifest.e69fd3aca104d7348d84d490a8ec93bf.js"
 );
 
 workbox.clientsClaim();
