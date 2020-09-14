d96b80160c3939ef1d86bf20581730ab8bfb06df
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-14 15:09:09 +0530

    Update Country.js

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,14 +3,7 @@ import { NavLink } from 'react-router-dom';
 import "../style/Country.scss";
 
 const Country = (props) => {
-        const answers = props.borders.map((answer, index) => (
-        <li className="borders-item" key={index}>
-            <NavLink to={answer} className="navLink">
-                <button className="btn btn--borders">{answer}</button>
-            </NavLink>
-        </li>
-    ))
-    
+        
     return (
         <div className="country-container">
             <div className="country-container__back">
@@ -46,7 +39,7 @@ const Country = (props) => {
                         <ul className="info-list__borders">
                             <h4>Border Countries:</h4>
                                  {props.borders}
-                                    {answers}
+                                    
                            
                         </ul>
                     </ul>
