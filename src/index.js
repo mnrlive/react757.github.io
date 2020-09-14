d6da9e3e515f13d29b3da5064ed6e142e162d68e
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-14 16:56:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import Router from "./components/Router";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
