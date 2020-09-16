d96d1793f27a7814442438fd5b1c7e9ee4640f04
Pandimadhubabu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-16 07:57:34 +0000

    Deploying to master from  @ f9825f8c96018784d8e3a0437e522d0b555ed6b7 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d4eaa924d60c5cd8258cfe1df48a9db.js"
+  "/precache-manifest.a74e3069740fd00cf70ebbcacd47ac39.js"
 );
 
 workbox.clientsClaim();
